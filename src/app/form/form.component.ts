e040f58eff74c334c1502a7a5fc2a893e617f775
uwamahororachel <racheluwamahoro55@gmail.com>
2020-11-07 06:00:11 +0200

    additional codes for form

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -6,3 +6,46 @@ import { reject } from 'q';
 import { Userview } from '../userview';
 import { Repolist } from '../repolist';
 import { from } from 'rxjs';
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent implements OnInit {
+
+  report: Userview;
+  userName :string;
+  reponse : any;
+  repolist: Repolist[];
+  constructor(private http: HttpClient,private raissa:UserserviceService ) {
+
+   }
+
+  ngOnInit() {
+  }
+  search (){
+    this.raissa.userService(this.userName);
+    this.report=this.raissa.user;
+    console.log(this.report) 
+
+    this.raissa.userService(this.userName);
+    this.report=this.raissa.user;
+    console.log(this.report) 
+  //   let promise = new Promise ((resolve , reject) => {
+  // this.http.get('https://api.github.com/users/'+this.userName + "?access_token=d01cb9548cffe04e59b685700487958bc798a53a")
+  // .subscribe((reponse) => {
+  //   this.reponse = reponse;
+  //   console.log(this.reponse)
+  //   resolve () 
+  // },
+  // err => {
+  //     this.reponse.login="Invalid "
+
+  //  reject()
+  // })
+
+  // })
+
+}
+
+}
