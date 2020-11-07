5efb58db1e374bba3bde7f1ef641a9d2146dd7cf
uwamahororachel <racheluwamahoro55@gmail.com>
2020-11-07 06:03:24 +0200

    Adding the repolist files

---
+++ src/app/form/repolist.ts
@@ -0,0 +1,3 @@
+export class Repolist {
+    constructor( public name:string, public description: string){}
+}
---
+++ src/app/repolist.spec.ts
@@ -0,0 +1,7 @@
+import { Repolist } from './repolist';
+
+describe('Repolist', () => {
+  it('should create an instance', () => {
+    expect(new Repolist()).toBeTruthy();
+  });
+});
