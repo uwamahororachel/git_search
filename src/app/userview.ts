c71aee296d906c1dfa9e7630efc058ec8ca8122a
uwamahororachel <racheluwamahoro55@gmail.com>
2020-11-07 06:41:17 +0200

    setting up variables to withdrwa the data

---
+++ src/app/userview.ts
@@ -0,0 +1,12 @@
+export class Userview {
+    constructor(
+        public avatar_url:string ,
+        public login:string, 
+        public public_repos:number ,
+        public followers:number, 
+        public following:number , 
+        public bio:string,
+        public html_url:string){
+    }
+}
+
